5de9636ad6cc6f346b1419991de1a44ddebe6766
James von Seggern <yityoo@frontier.com>
2024-10-19 12:08:13 -0700

    move music to loop

--- code.py
+++ code.py
@@ -20,18 +20,23 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c)
 lis3dh.range = adafruit_lis3dh.RANGE_4_G
 
-
+# The listed mp3files will be played in order
+mp3files = ["slow.mp3", "happy.mp3"]
 
 while True:
     if lis3dh.shake(shake_threshold=12):
         print("Shaken!")
+        mp3 = open(mp3files[0], "rb")
+        decoder = audiomp3.MP3Decoder(mp3)
+        audio.play(decoder)
+        while audio.playing:
+           pass
     # Small delay to keep things responsive but give time for interrupt processing.
     time.sleep(0.1)
    # x, y, z = lis3dh.acceleration
    # print((x, y, z))
 
-# The listed mp3files will be played in order
-mp3files = ["slow.mp3", "happy.mp3"]
+
 
 # You have to specify some mp3 file when creating the decoder
 mp3 = open(mp3files[0], "rb")
